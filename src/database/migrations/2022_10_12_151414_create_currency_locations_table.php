721a0e95c831a2a259823740356194797118f325
CezarGR <51181637+CezarGR@users.noreply.github.com>
2022-10-12 15:32:10 -0300

    feat: Add Currency model

---
+++ src/database/migrations/2022_10_12_151414_create_currency_locations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency_locations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('currency_id')->constrained('currencies');
+            $table->string('location');
+            $table->string('icon')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency_locations');
+    }
+};
